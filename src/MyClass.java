b9401014bd94ff2a5764afb4542f6065787a9f80
BenSidiAlessio <bensidialessio@gmail.com>
2021-02-06 15:25:39 +0100

    First Commit :)

---
+++ src/MyClass.java
@@ -0,0 +1,16 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String helloWorld = "helloWorld";
+		boolean primoProgramma = true;
+		
+		if(primoProgramma) {
+			System.out.println(helloWorld);
+		}else {
+			System.out.println("there is nothing here");
+		}
+	}
+
+}
